bcbe5d0bb64c27863dc0039e5c2f8e02641cb70a
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-10-12 16:26:05 -0300

    create layout and style of the orphanages map
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
   return (
-    <h1>Hello World!</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>SP</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
   );
 }
 
