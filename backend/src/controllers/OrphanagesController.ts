3a193c754eb78c5f7e799b14da171631665d1f25
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-10-13 13:16:18 -0300

    create controllers that wrap entities methods
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+const OrphanagesController = {
+  async create(request: Request, response: Response)  {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = request.body;
+  
+    const orphanagesRepository = getRepository(Orphanage);
+  
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    });
+  
+    await orphanagesRepository.save(orphanage);
+  
+    return response.status(201).json(orphanage);
+  }
+};
+
+export default OrphanagesController;
